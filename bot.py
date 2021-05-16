7ca280ab1f16c34050d48697dc77f3a858efcb14
Estol75 <64350850+Estol75@users.noreply.github.com>
2021-05-16 14:34:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ import random
 from random import choice
 import fake_useragent
 
-
+if fdsf:
 
 
 user = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
