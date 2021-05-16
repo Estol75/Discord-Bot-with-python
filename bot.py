9b062efe67eb7a85287965ab767da8320c7b14ad
Estol75 <64350850+Estol75@users.noreply.github.com>
2021-05-16 14:27:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ import fake_useragent
 
 
 
-user = "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)"
+user = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 header = {'user-agent': user}
 
 
