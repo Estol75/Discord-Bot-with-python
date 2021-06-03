63921febcd8de4f198202b5971c3c0a1315290ae
Estol75 <64350850+Estol75@users.noreply.github.com>
2021-06-03 10:46:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ Bot.remove_command('help')
 
 @Bot.event
 async def on_ready():
-    await Bot.change_presence(activity=discord.Game(name="q.help v1.0.49"))
+    await Bot.change_presence(activity=discord.Game(name="q.help v1.0.59"))
 
 
 cluster = MongoClient("mongodb://Estol:JWTZW7UrQSeTFYVY@cluster0-shard-00-00.bjidh.mongodb.net:27017,cluster0-shard-00-01.bjidh.mongodb.net:27017,cluster0-shard-00-02.bjidh.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-12d4np-shard-0&authSource=admin&retryWrites=true&w=majority")
