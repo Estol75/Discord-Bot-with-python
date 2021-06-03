3e5944937dbecdcaea7c9d79050f91c6dcca38f0
Estol75 <64350850+Estol75@users.noreply.github.com>
2021-06-03 14:51:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ Bot.remove_command('help')
 
 @Bot.event
 async def on_ready():
-    await Bot.change_presence(activity=discord.Game(name="q.help v1.0.79"))
+    await Bot.change_presence(activity=discord.Game(name="q.help v1.0.69"))
 
 
 mongo = os.environ.get('MONGO')
