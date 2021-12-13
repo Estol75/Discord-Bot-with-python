1a7799866470a302911f2e3d7f234245d952570b
Estol75 <64350850+Estol75@users.noreply.github.com>
2021-12-13 16:01:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -791,96 +791,96 @@ async def invite(ctx):
 
 
 
-@Bot.command()
-async def aktie(ctx, arg1):
-
-    #подключения драйвера
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(service=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    
-
-    
-    len_link = f"https://www.google.com/search?q={arg1}+aktie"
-    driver.get(f"https://www.google.com/search?q={arg1}+aktie")
-    driver.maximize_window()
-    sleep(2)
-    driver.find_element(By.ID, "L2AGLb").click()
-
-    screenshot = driver.save_screenshot('my_screenshot.png')
-    sleep(2)
-
-    sleep(2)
-    im = Image.open('my_screenshot.png')
-    sleep(2)
-    im_crop = im.crop((20, 365, 700, 550))
-
-    im_crop.save('guido_pillow_crop.png', quality=95)
-    sleep(1)
-
-
-    elem= driver.find_element_by_css_selector(".NprOob")
-    elems= driver.find_element_by_css_selector(".WlRRw")
-
-    textil = elems.text
-    lents = len(elems.text)
-    bin = int(lents) - int(5)
-    rel = textil[0:bin]
-    print(textil[0:1])
-
-    if str(textil[0:1]) == str("+"):
-        embed_en = discord.Embed(title=f"▬▬▬▬▬▬▬▬[Акции {arg1}]▬▬▬▬▬▬▬▬", description=f"**Стоимость:** {elem.text} │ **просадок:** {rel}", color=0x3cd126)
-    else:
-        embed_en = discord.Embed(title=f"▬▬▬▬▬▬▬▬[Акции {arg1}]▬▬▬▬▬▬▬▬", description=f"**Стоимость:** {elem.text} │ **просадок:** {rel}", color=0xea4335)
-
-
-
-#     file = discord.File(r"guido_pillow_crop.png", filename="guido_pillow_crop.png")
-    await ctx.send(file=discord.File("guido_pillow_crop.png"))
-#     embed_en.set_image(url="attachment://guido_pillow_crop.png")
-    embed_en.set_image(url=discord.File("guido_pillow_crop.png"))
-    
-    await ctx.send(file=file, embed=embed_en)
-    os.remove("my_screenshot.png")
-    os.remove("guido_pillow_crop.png")
-
-#     .find('li', class_ = "pager__item pager__item_last-page")
-
-
-
 # @Bot.command()
 # async def aktie(ctx, arg1):
+
 #     #подключения драйвера
 #     chrome_options = webdriver.ChromeOptions()
 #     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 #     chrome_options.add_argument("--headless")
 #     chrome_options.add_argument("--disable-dev-shm-usage")
 #     chrome_options.add_argument("--no-sandbox")
-#     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+#     driver = webdriver.Chrome(service=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     
-    
-#     driver.get(f"https://www.google.com/search?q={arg1}+aktie")
 
-#     sleep(1)
-#     elem= driver.find_element_by_css_selector(".NprOob")
-#     print(elem.text)
     
+#     len_link = f"https://www.google.com/search?q={arg1}+aktie"
+#     driver.get(f"https://www.google.com/search?q={arg1}+aktie")
+#     driver.maximize_window()
+#     sleep(2)
+#     driver.find_element(By.ID, "L2AGLb").click()
+
 #     screenshot = driver.save_screenshot('my_screenshot.png')
-    
+#     sleep(2)
+
+#     sleep(2)
 #     im = Image.open('my_screenshot.png')
-#     sleep(1)
+#     sleep(2)
 #     im_crop = im.crop((20, 365, 700, 550))
-#     sleep(1)
+
 #     im_crop.save('guido_pillow_crop.png', quality=95)
-    
+#     sleep(1)
+
+
+#     elem= driver.find_element_by_css_selector(".NprOob")
+#     elems= driver.find_element_by_css_selector(".WlRRw")
+
+#     textil = elems.text
+#     lents = len(elems.text)
+#     bin = int(lents) - int(5)
+#     rel = textil[0:bin]
+#     print(textil[0:1])
+
+#     if str(textil[0:1]) == str("+"):
+#         embed_en = discord.Embed(title=f"▬▬▬▬▬▬▬▬[Акции {arg1}]▬▬▬▬▬▬▬▬", description=f"**Стоимость:** {elem.text} │ **просадок:** {rel}", color=0x3cd126)
+#     else:
+#         embed_en = discord.Embed(title=f"▬▬▬▬▬▬▬▬[Акции {arg1}]▬▬▬▬▬▬▬▬", description=f"**Стоимость:** {elem.text} │ **просадок:** {rel}", color=0xea4335)
+
+
+
+# #     file = discord.File(r"guido_pillow_crop.png", filename="guido_pillow_crop.png")
 #     await ctx.send(file=discord.File("guido_pillow_crop.png"))
+# #     embed_en.set_image(url="attachment://guido_pillow_crop.png")
+#     embed_en.set_image(url=discord.File("guido_pillow_crop.png"))
+    
+#     await ctx.send(file=file, embed=embed_en)
 #     os.remove("my_screenshot.png")
+#     os.remove("guido_pillow_crop.png")
+
+# #     .find('li', class_ = "pager__item pager__item_last-page")
+
+
+
+@Bot.command()
+async def aktie(ctx, arg1):
+    #подключения драйвера
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    
+    
+    driver.get(f"https://www.google.com/search?q={arg1}+aktie")
+    driver.maximize_window()
+    sleep(1)
+    elem= driver.find_element_by_css_selector(".NprOob")
+    print(elem.text)
+    
+    screenshot = driver.save_screenshot('my_screenshot.png')
+    
+    im = Image.open('my_screenshot.png')
+    sleep(1)
+    im_crop = im.crop((20, 365, 700, 550))
+    sleep(1)
+    im_crop.save('guido_pillow_crop.png', quality=95)
+    
+    await ctx.send(file=discord.File("guido_pillow_crop.png"))
+    os.remove("my_screenshot.png")
    
     
-#     await ctx.send(elem.text)
+    await ctx.send(elem.text)
     
 
 
